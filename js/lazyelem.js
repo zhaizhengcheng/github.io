977449c95f48f20fe29c6917e0d89ca2cfc0c55c
大王叫我来巡山 <943221383@qq.com>
2016-07-06 16:06:52 +0800

    Create lazyelem.js

--- js/lazyelem.js
+++ js/lazyelem.js
@@ -1,1 +1,269 @@
-!function(a,b){"use strict";var c,d,e=b(a),f=!1,g=[],h=[],i={timeout:10,buffer:100,loadingClass:"lazy-loading",srcValue:"lazy-src",bgValue:"lazy-bg"},j={_init:function(){c=this},listen:function(a,d,e){var j,k=b(a||"img["+i.srcValue+"]"),l=d||"img";e&&(j=h.push(e)-1),k.each(function(){var a=b(this);g.push({type:l,obj:a,callback:e?h[j]:null}),a.addClass(i.loadingClass)}),f||c._startListen(),c.detect()},_startListen:function(){e.bind("scroll.lazyelem",function(){clearTimeout(d),d=setTimeout(function(){c.detect()},i.timeout)}),e.bind("resize.lazyelem",function(){c.detect()}),f=!0},detect:function(){for(var a=0;a<g.length;a++){var b=g[a],d=b.obj;if(!d.is(":hidden")&&c._isInScreen(d)){switch(b.type){case"fn":break;case"img":d.attr("src",d.attr(i.srcValue)).removeAttr(i.srcValue);break;case"bg":d.css("background-image","url("+d.attr(i.bgValue)+")").removeAttr(i.bgValue);break;case"dom":var f=d.children("script");f.replaceWith(c._minHtml(f.html()))}b.callback&&b.callback(d),d.removeClass(i.loadingClass),g.splice(a--,1)}}0===g.length&&e.unbind("scroll.lazyelem resize.lazyelem")},_isInScreen:function(a){var b=e.height(),c=e.scrollTop(),d=a.height(),f=a.offset().top;return f+d>c-i.buffer&&f<c+b+i.buffer},_minHtml:function(a){var b=/\n+/g,c=/<!--.*?-->/gi,d=/\/\*.*?\*\//gi,e=/[ ]+</gi;return a=a.replace(b,""),a=a.replace(c,""),a=a.replace(d,""),a=a.replace(e,"<")},config:function(a){b.extend(i,a)}};j._init(),a.lazyelem=j}(window,jQuery);
+/*!
+ * lazyelem - lazyloader for html elements
+ * Author: LiXiao
+ * Released under MIT license
+ */
+
+;(function(window, $) {
+
+    'use strict';
+
+    var $win = $(window),
+
+        // 滚动监听是否开启
+        isListening = false,
+
+        // 被监听的元素集合
+        listeners = [],
+        groups = {},
+
+        // 回调函数集合
+        callbacks = [],
+
+        // 默认配置
+        config = {
+            timeout: 10,
+            buffer: 100,
+            loadingClass: 'lazy-loading',
+            srcValue: 'lazy-src',
+            bgValue: 'lazy-bg'
+        },
+
+        that, timer;
+
+
+    /**
+     * 全局主对象
+     * @module lazyelem
+     */
+    var lazyelem = {
+
+        /**
+         * 初始化框架
+         * @method _init
+         */
+        _init: function() {
+            that = this;
+        },
+
+        /**
+         * 添加监听元素
+         * @method listen
+         * @param {String|Object} selector 用于选中元素的jQuery选择器,或直接传入一组jQuery对象
+         * @param {String} type 元素类型，可用值 'img'|'bg'|dom'|'fn'
+         * @param {Function} callback 加载后的回调函数 参数1:当前触发加载条件的jquery对象
+         */
+        listen: function(selector, type, callback) {
+            var elements = selector,
+                eType = (type || 'img').toLowerCase(),
+                cbIndex;
+
+            // 判断传入的是否选择器
+            if ((typeof selector).toLowerCase() != 'object') {
+                elements = $(selector || 'img[' + config.srcValue + ']');
+            }
+
+            // 注册回调函数
+            if (callback) {
+                cbIndex = callbacks.push(callback) - 1;
+            }
+            // 添加批处理组
+            if (eType === 'bat') {
+                groups[cbIndex] = {
+                    objs: [],
+                    cbIndex: cbIndex
+                };
+            }
+
+            // 添加监听元素
+            elements.each(function() {
+                var o = $(this);
+
+                listeners.push({
+                    type: eType,
+                    obj: o,
+                    cbIndex: cbIndex
+                });
+
+                // 加上loading class
+                if (eType == 'img') {
+                    o.addClass(config.loadingClass);
+                }
+            });
+
+            // 启动窗口滚动事件监听
+            if (!isListening) {
+                that._startListen();
+            }
+
+            that.detect();
+        },
+
+        /**
+         * 绑定窗口监听事件
+         * @method _startListen
+         */
+        _startListen: function() {
+            $win.bind('scroll.lazyelem resize.lazyelem', function() {
+                if (!listeners.length) {
+                    return;
+                }
+
+                clearTimeout(timer);
+                
+                timer = setTimeout(function() {
+                    that.detect();
+                }, config.timeout);
+            });
+
+            isListening = true;
+        },
+
+        /**
+         * 检测所有元素是否满足加载条件
+         * @method detect
+         */
+        detect: function() {
+            for (var i = 0; i < listeners.length; i++) {
+                var listener = listeners[i],
+                    obj = listener.obj,
+                    cbIndex = listener.cbIndex;
+
+                // 以下情况直接跳过本次循环： 元素不可见 | 不在屏幕区域内
+                if (that._isHidden(obj) || !that._isTrigger(obj)) {
+                    continue;
+                }
+
+                // 开始加载
+                switch(listener.type) {
+                    case 'fn':
+                        break;
+
+                    case 'img':
+                        var src = obj.attr(config.srcValue);
+                        src && obj.attr('src', src).removeAttr(config.srcValue);
+                        break;
+
+                    case 'bg':
+                        var bg = obj.attr(config.bgValue);
+                        bg && obj.css('background-image', 'url(' + bg + ')').removeAttr(config.bgValue);
+                        break;
+
+                    case 'dom':
+                        var script = obj.children('script');
+                        script.length && script.replaceWith(that._minHtml(script.html()));
+                        break;
+
+                    case 'bat':
+                        groups[cbIndex].objs.push(obj);
+                        break;
+                }
+
+                // 删除该项
+                listeners.splice(i--, 1);
+
+                // 调用回调函数
+                if (listener.type !== 'bat' && cbIndex >= 0) {
+                    callbacks[cbIndex](obj);
+                }
+            }
+
+            // 批处理
+            that._bat();
+
+            // 当所有元素加载完毕，停止监听
+            if (listeners.length === 0) {
+                $win.unbind('scroll.lazyelem resize.lazyelem');
+                isListening = false;
+            }
+        },
+
+        /**
+         * 批量处理懒加载元素
+         * @method _bat
+         */
+        _bat: function() {
+            for (var g in groups) {
+                var objs, cb;
+                if (groups.hasOwnProperty(g)) {
+                    objs = groups[g].objs;
+                    cb = callbacks[groups[g].cbIndex];
+
+                    if (objs.length) {
+                        cb(objs);
+                        groups[g].objs = [];
+                    }
+                }
+            }
+        },
+
+        /**
+         * 判断一个jquery对象是否在屏幕区域之间
+         * @method _isTrigger
+         * @param {jQuery Object} obj 单个jquery对象
+         * @return {Boolean}
+         */
+        _isTrigger: function(obj) {
+            var winHeight = $win.height(),
+                winTop = $win.scrollTop(),
+                oHeight = obj.height(),
+                ot = obj.offset().top;
+
+            return (ot + oHeight) > (winTop - config.buffer) && ot < (winTop + winHeight + config.buffer);
+        },
+
+        /**
+         * 判断一个jquery对象是否可见
+         * @method _isHidden
+         * @param {jQuery Object} obj 单个jquery对象
+         * @return {Boolean}
+         */
+        _isHidden: function(obj) {
+            var elem = obj[0];
+
+            if (obj.css('display') === 'none' || !$.contains(elem.ownerDocument, elem )) {
+                return true;
+            }
+            if (elem.offsetWidth <= 0 && elem.offsetHeight <= 0) {
+                return true;
+            }
+            return false;
+        },
+
+        /**
+         * 最小化一段html文本
+         * @method _minHtml
+         * @param {String} html 一段html代码文本
+         * @return {String} 处理后的html文本
+         */
+        _minHtml: function(html) {
+            var rep = /\n+/g,
+                repone = /<!--.*?-->/ig,
+                reptwo = /\/\*.*?\*\//ig,
+                reptree = /[ ]+</ig;
+
+            html = html.replace(rep, '');
+            html = html.replace(repone, '');
+            html = html.replace(reptwo, '');
+            html = html.replace(reptree, '<');
+
+            return html;
+        },
+
+        /**
+         * 配置参数
+         * @method config
+         * @param {Object} opt 配置项 timeout|buffer|loadingClass|srcValue|bgValue
+         */
+        config: function(opt) {
+            $.extend(config, opt);
+        }
+    };
+
+    // 初始化
+    lazyelem._init();
+
+    // 附加到全局对象
+    window.lazyelem = lazyelem;
+
+})(window, window.jQuery || window.Zepto);
